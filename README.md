4fd46360684bb2bb28ae368d010c1e944ed64b2d
Augustus1110 <donahi22@gmail.com>
2023-06-20 06:04:08 -0700

    Added new screen shot, added README

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Wonderful-Weather-Dashboard
+# Wonderful Weather Dashboard
+
+This project is a weather dashboard application that allows users to search for a city and view the current weather conditions as well as the 5-day forecast for that city. The application is built using HTML, CSS, and JavaScript.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+
+## Features
+
+- City Search: Users can search for a city using the input field provided.
+- Current Weather: The application displays the current weather conditions for the searched city, including the city name, date, weather condition icon, temperature, humidity, and wind speed.
+- 5-Day Forecast: The application provides a 5-day forecast for the searched city, displaying the date, weather condition icon, temperature, humidity, and wind speed for each day.
+- Search History: The application keeps track of the user's search history and displays past searched cities as buttons. Clicking on a past city button retrieves and displays the current and future weather conditions for that city.
+- Responsive Design: The application is designed to be responsive and can adapt to different screen sizes, making it accessible on various devices.
+
+## Getting Started
+
+To run this project locally, follow these steps:
+
+1. Clone the repository: `git clone <https://github.com/your-username/weather-dashboard.git>`
+2. Navigate to the project directory: `weather-dashboard`
+3. Open the `index.html` file in a web browser.
+
+## Usage
+
+1. Open the weather dashboard application in a web browser.
+2. Enter a city name in the search input field and click the search button or press Enter.
+3. The current weather conditions and the 5-day forecast for the entered city will be displayed.
+4. To view the current and future weather for a past searched city, click on the respective city button in the search history.
+5. Repeat the above steps to search for weather conditions of different cities.
+
+## Screenshots
+
+![Weather Dashboard](weather-dashboard.png)
+
+## Credits
+
+This project was developed by Donahi Casas-Silva. It was created as part of the UC Berkeley Extension Coding Bootcamp curriculum and is based on the requirements in the instructional materials.
+
+### Instructors/Tutors/Collaborators:
+
+This scheduler was created with help from the following people and resources:
+UC Berkeley Extension Bootcamp course instructor, Teaching Assistants and Zoom video tutors
--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ var fiveDayWeather = document.getElementById("five_day_weather");
 var fiveDayContainer = document.getElementById("five_day_container");
 var pastCityContainer = document.getElementById("past_city_container");
 
-//Event listener for the submit button, when the user clicks the Submit button, the getCurrentCoordinates function will run
+// Event listener for the submit button, when the user clicks the Submit button, the getCurrentCoordinates function will run
 
 submitButton.addEventListener("click", function () {
   getCurrentCoordinates(userInput.value);
@@ -26,19 +26,19 @@ userInput.addEventListener("keydown", function (event) {
   }
 });
 
-//All API calls and keys are from OpenWeatherMap.org
+// All API calls and keys are from OpenWeatherMap.org
 
-//This is my unique current weather API key
-//b12be20cab068cdc779326a35a3ed4c2
+// This is my unique current weather API key
+// b12be20cab068cdc779326a35a3ed4c2
 
-//Current weather geocoding API call
+// Current weather geocoding API call
 // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-//Current weather latitude and longitude API call
-//api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}//
+// Current weather latitude and longitude API call
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}//
 
 
-//I have to write a function that will get the coordinates of the city that the user inputs
+// Function that will get the coordinates of the city that the user inputs
 function getCurrentCoordinates(city) {
   var url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=b12be20cab068cdc779326a35a3ed4c2";
   fetch(url)
@@ -57,7 +57,7 @@ function getCurrentCoordinates(city) {
     });
 }
 
-//I have to write a function that will get the current weather based on the coordinates
+// Function that will get the current weather based on the coordinates
 function getCurrentWeather(lat, lon) {
   var url = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=b12be20cab068cdc779326a35a3ed4c2&units=imperial";
   fetch(url)
@@ -70,7 +70,7 @@ function getCurrentWeather(lat, lon) {
     });
 }
 
-//I have to write a function will display the current weather
+// Function will display the current weather and matching icon
 var showCurrentWeather = function (data) {
     city.textContent = data.name;
     date.innerHTML = dayjs.unix(data.dt).format("dddd") + "<br>" + "<br>" + dayjs.unix(data.dt).format("MMMM D, YYYY");
@@ -80,16 +80,16 @@ var showCurrentWeather = function (data) {
     document.getElementById('icon').innerHTML = `<img src='http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png'/>`
   };
   
-//5-Day Geocoding API call
-//api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+// 5-Day Geocoding API call
+// api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 
-//5-Day Personal Key
-//b12be20cab068cdc779326a35a3ed4c2
+// 5-Day Personal Key
+// b12be20cab068cdc779326a35a3ed4c2
 
-//5-Day Lat Long API call
-//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+// 5-Day Lat Long API call
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
-//I have to write a function that will get the 5 day forecast based on the coordinates
+// Function that will get the 5 day forecast based on the coordinates
 function getFiveDayForecast(lat, lon) {
   var url = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=b12be20cab068cdc779326a35a3ed4c2&units=imperial";
   fetch(url)
@@ -101,7 +101,7 @@ function getFiveDayForecast(lat, lon) {
     })
 }
 
-//I have to write a function that will display the 5 day forecast
+// Function that will display the 5 day forecast and display icons accordingly
 var showFiveDayForecast = function (data) {
   fiveDayContainer.innerHTML = "";
   var forecastRow = document.createElement("div");
@@ -126,8 +126,8 @@ var showFiveDayForecast = function (data) {
     fiveDayContainer.appendChild(forecastRow);
   };
 
-//check local storage when the page loads
-//if something is in local storage, get it and display it
+
+// Function that will display the past cities that the user has searched for
 
 function showPastCities() {
   pastCityContainer.innerHTML = "";
@@ -152,5 +152,3 @@ if (cityStorageRaw) {
 else {
   cityStorage = [];
 }
-
-
